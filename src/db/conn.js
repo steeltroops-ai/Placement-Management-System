58a4e47e579569e7dedc7b35b826f1202f6b71aa
Mayank Pratap Singh <87810082+steeltroops-ai@users.noreply.github.com>
2023-06-15 16:08:09 +0530

    resume

--- src/db/conn.js
+++ src/db/conn.js
@@ -8,5 +8,5 @@ mongoose.connect("mongodb://localhost:27017/Placement-Manager", {
 }).then(() => {
   console.log("Connected to MongoDB");
 }).catch((error) => {
-  console.log("Error connecting to MongoDB:");
+  console.log("Error connecting to MongoDB:",error);
 });
